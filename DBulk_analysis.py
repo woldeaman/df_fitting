caeadbf1bc576db1284a903aa11c95f3be6d95b2
woldeaman <AmanuelWK@Amanuels-MacBook.local>
2018-05-25 20:20:31 +0200

    now indicate fixed DSol in figures

--- DBulk_analysis.py
+++ DBulk_analysis.py
@@ -6,6 +6,7 @@ import mpltex  # for acs style figures
 
 # change home directory accordingly
 home = '/Users/woldeaman/'
+home = '/Users/AmanuelWK/'
 
 
 # script estimates experimental error and compares it to error for different
@@ -15,17 +16,20 @@ home = '/Users/woldeaman/'
 ##########################################################################
 @mpltex.acs_decorator  # making acs-style figures
 def figure_diffusivities(d_sol, d_gel, f_gel, error, name='params_dsol',
-                         title='', save=False, scale='lin'):
+                         title='', save=False, scale='lin', opti=None):
     """Plot parameters change with d_sol."""
     fig, axes = plt.subplots(3, 1, sharex='col')
     axes[0].plot(d_sol, error, 'ko')
-    axes[0].axhline(error[-1], c='k', ls=':')
+    if opti is not None:
+        axes[0].axvline(opti, c='k', ls=':')
     axes[0].set(ylabel="Minimal error $\sigma$", title=title)  # add title if prefered
     axes[1].plot(d_sol, d_gel, 'ro')
-    axes[1].axhline(np.average(d_gel), ls=':', c='r')
+    if opti is not None:
+        axes[1].axvline(opti, ls=':', c='r')
     axes[1].set(ylabel="D$_{gel}$ [$\mu$m$^2$/s]")
     axes[2].plot(d_sol, f_gel, 'bo')
-    axes[2].axhline(np.average(f_gel), ls=':', c='b')
+    if opti is not None:
+        axes[2].axvline(opti, ls=':', c='b')
     axes[2].set(xlabel="D$_{bulk}$ [$\mu$m$^2$/s]",
                 ylabel="$\Delta$F$_{gel}$ [k$_B$T]")
     if scale is 'log':
@@ -48,7 +52,7 @@ def read_data(path):
     for set in setups:
         D_sol[set], D_gel[set], F_gel[set], Error[set] = [], [], [], []
         for d in diffusivities:
-            subpath = '%s/DSol_%s/results_DSol=%.2f/' % (set, d, d)
+            subpath = '%s/DSol_%s/results_DSol=%.6f/' % (set, d, d)
             err_raw = np.loadtxt(path+subpath+'minError.txt', delimiter=',')
             df_raw = np.loadtxt(path+subpath+'DF_best.txt', delimiter=',')
             min_err = np.min(err_raw)  # gather minimal error
@@ -69,17 +73,18 @@ def read_data(path):
 ##########################################################################
 setups = ['gel10_dex10', 'gel10_dex4', 'gel6_dex10', 'gel6_dex20', 'gel6_dex4']
 diffusivities = [d for d in np.arange(100, 1001, 100).astype(int)] + [1, 27.4, 39.5, 55, 101.4]
-path_d_data = home+"/Desktop/Cluster/jobs/fokkerPlanckModel/Block_data/reflective_full_bulk/sigmoidal/"
+path_d_data = home+"/Desktop/Cluster/jobs/fokkerPlanckModel/Block_data/c0_const/sigmoidal/"
 ##########################################################################
 
 
 #################################
 #             MAIN LOOP         #
 ##########################################################################
+DSols = [55, 101.4, 55, 39.5, 101.4]  # chosen DSol for different dextrans
 # read errors for different d values
 D_sol, D_gel, F_gel, Error = read_data(path_d_data)
 
 for i, set in enumerate(setups):
-    figure_diffusivities(D_sol[set], D_gel[set], F_gel[set], Error[set],
+    figure_diffusivities(D_sol[set], D_gel[set], F_gel[set], Error[set], opti=DSols[i],
                          title=set.split('_'), name=set, save=True)
 ##########################################################################
