43e99f4a0038ef6331a3cfa965b06f9045ce1d61
woldeaman <AmanuelWK@Amanuels-MacBook.local>
2018-06-07 23:44:35 +0200

    average only over top 10% of runs

--- DiffusionEq_BlockReflecting_sigmoidal_scale_DSolFree.py
+++ DiffusionEq_BlockReflecting_sigmoidal_scale_DSolFree.py
@@ -372,7 +372,7 @@ def main():
         print('\nDoing analysis only.')
         res = np.load('result.npy')
         print('Overall %i runs have been performed.' % res.size)
-        analysis(np.array(res), xx, cc, tt, dxx_dist, dxx_width, per=1)
+        analysis(np.array(res), xx, cc, tt, dxx_dist, dxx_width, per=0.1)
         print('\nPlots have been made and data was extraced and saved.')
         sys.exit()
 
@@ -387,7 +387,7 @@ def main():
             print('\n\nScript has been terminated.\nData will now be analyzed...')
             break
 
-    analysis(np.array(results), xx, cc, tt, dxx_dist, dxx_width, per=1)
+    analysis(np.array(results), xx, cc, tt, dxx_dist, dxx_width, per=0.1)
 
     return runs  # returns number of runs in order to compute average time per run
 
