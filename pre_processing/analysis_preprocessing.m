13d22369a17c89ccc7897ca550480d5b8ca8892c
Amanuel Wolde-Kidan <woldeaman@hiraiwamac.physik.fu-berlin.de>
2018-07-26 18:08:14 +0200

    added normalization to preprocessing

--- pre_processing/analysis_preprocessing.m
+++ pre_processing/analysis_preprocessing.m
@@ -1,11 +1,13 @@
 % pre processing of profiles, flip and cut glass off
 
+name = 'gel6_dex70_forts'  % supply name for profiles
 % first flip to have bulk on left side
 flipped = transpose(fliplr(transpose(int(:, :))));
 
 % then cut off glass, adjust cut-off bin here
-cut_off = 50;
+cut_off = 40;
 cut = flipped(1:cut_off, :);
+cut = cut./cut(1);  % set concentration to one at leftmost bin in the beginning
 
 % create x-vector
 dx = 10;
@@ -20,4 +22,4 @@ processed(:, 1) = xx;
 processed(:, 2:length(cut)+1) = cut;
 
 % save to file
-dlmwrite('/Users/woldeaman/Desktop/data.txt', processed, 'delimiter', ',');
+dlmwrite(strcat('/Users/woldeaman/Desktop/', name, '.txt'), processed, 'delimiter', ',');
--- pre_processing/generate_profiles.m
+++ pre_processing/generate_profiles.m
@@ -1,9 +1,10 @@
 % exemplary script showing pre-processing steps of microscopy data
 clear;
 
-filename='';  % supply filenames here
+data_path='/Users/woldeaman/Downloads/180724-ah202-g6-d70_forts/'; % supply path to data
+filename='180724-ah202.lif_180724-ah202-g6-d70 forts';  % supply filenames here
 z_max=44;  % number of recorde z-stacks
-t_max=99;  % number of recorded time points
+t_max=57;  % number of recorded time points
 
 int=[];
 
@@ -27,7 +28,7 @@ for it=0:t_max
         file_ext_z=strcat('_z',file_ext_z,'_ch00.tif');
         base_str_in=strcat(filename,file_ext_t,file_ext_z);
 
-        tmp=double(imread(strcat('./_raw/',base_str_in),1));
+        tmp=double(imread(strcat(data_path,base_str_in),1));
         % NOTE: this is an edge correction,
         % if activated discarding values at the image boundaries
 %         tmp=tmp(10:end-10,10:end-10);
@@ -36,9 +37,9 @@ for it=0:t_max
 
     % NOTE: this is a baseline correction,
     % intensity in glass should be zero, substract any non zero contributions
-   %int_tmp=int_tmp-mean(int_tmp(1:7));
-%    pp=polyfit((length(int_tmp)-12:length(int_tmp)),int_tmp(end-12:end)',1);
-%    int_tmp=int_tmp./polyval(pp,(1:length(int_tmp)))';
+   int_tmp=int_tmp-mean(int_tmp(1:7));
+   % pp=polyfit((length(int_tmp)-12:length(int_tmp)),int_tmp(end-12:end)',1);
+   % int_tmp=int_tmp./polyval(pp,(1:length(int_tmp)))';
    % NOTE: this is the normalization,
    % intensity increases over time, thats why we normalize to bulk value
    %int_tmp=int_tmp./mean(int_tmp(38:45));
