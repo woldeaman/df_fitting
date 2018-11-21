464484dbca7bbcec617c41bb41810d916ea18254
Amanuel Wolde-Kidan <woldeaman@hiraiwamac.physik.fu-berlin.de>
2018-11-21 17:03:49 +0100

    finished regularization script, eventhough approach seems not to be working...

--- post_processing/regularization.py
+++ post_processing/regularization.py
@@ -121,13 +121,31 @@ def plot_scalings(scalings_fit, scalings_test, tt_fit, tt_test, alphas, save=Fal
         plt.savefig(home+'/Desktop/scalings.pdf')
     else:
         plt.show()
+
+
+@mpltex.acs_decorator  # making acs-style figures
+def plot_test_set_err(alphas, test_err, sol_err, save=False):
+    """Make plot for performance on test set over alphas."""
+    plt.plot(alphas, test_err, 'k.--', label='test set')
+    plt.plot(alphas, sol_err, 'r.--', label='fitting set')
+    plt.xscale('log')
+    plt.xlabel('$\\alpha$')
+    plt.ylabel('$\sigma$')
+    # legend
+    plt.legend(frameon=False)
+
+    if save:
+        plt.tight_layout()
+        plt.savefig(home+'/Desktop/performance.pdf')
+    else:
+        plt.show()
 ##########################################################################
 
 
 ################################
 #    SETTING UP ENVIRONMENT    #
 ##########################################################################
-alphas = ['0.000001', '0.00001', '0.00005', '0.0001', '0.0005', '0.001', '0.005', '0.01']
+alphas = ['0', '0.000001', '0.00001', '0.00005', '0.0001', '0.0005', '0.001', '0.005', '0.01', '0.05', '0.1', '0.5']
 # NOTE: both tt-vectors should include t = 0 s !
 tt_fit = [0, 30, 60, 90, 120, 180, 240, 300, 330, 360, 390, 420, 480, 510, 540, 570, 630,
           660, 720, 750, 780, 810, 870, 900, 930, 960, 990, 1020, 1050, 1080, 1110, 1140,
@@ -136,7 +154,7 @@ tt_fit = [0, 30, 60, 90, 120, 180, 240, 300, 330, 360, 390, 420, 480, 510, 540, 
           2100, 2130, 2160, 2220, 2250, 2280, 2310, 2370, 2400, 2460, 2490, 2520, 2550,
           2580, 2610, 2640]
 tt_test = [0, 150, 210, 270, 450, 600, 690, 840, 1320, 1500, 1560, 1650, 1800, 2190, 2340, 2430]
-path = home+"/Desktop/Cluster/jobs/fokker_planck_modelling/Block_PEG/regularization/gel10_dex70/"
+path = home+"/Desktop/Cluster/jobs/fokkerPlanckModel/PEG_dextran/7.Batch/regularization/gel10_dex70/"
 ##########################################################################
 
 
@@ -146,17 +164,11 @@ path = home+"/Desktop/Cluster/jobs/fokker_planck_modelling/Block_PEG/regularizat
 cc_all, xx, dxx_dist, dxx_width = compute_exp_data(path+'/alpha_0/', 'gel10_dex70')
 err_sol, err_reg, parameters = read_results(path, alphas, xx, cc_all, dxx_dist, dxx_width, tt_fit, dt=30)
 
-len(tt_fit)+len(tt_test)
 scalings = [p[6:] for p in parameters]  # extract scaling factors from parameters
 test_scalings = get_test_set_scalings(scalings, tt_fit, tt_test)
 test_err = compute_test_performance(xx, dxx_dist, dxx_width, cc_all, parameters,
                                     tt_test, test_scalings, dt=30)
 
-test_err
-plt.plot(alphas[:-1], test_err[:-1])
 plot_scalings(scalings, test_scalings, tt_fit, tt_test, alphas, save=True)
-plot_lcurve(alphas, err_sol, err_reg)
-
-
-err_sol
-plt.plot(alphas, err_sol)
+plot_lcurve(np.array(alphas).astype(float), err_sol, err_reg, save=True)
+plot_test_set_err(np.array(alphas).astype(float), test_err, err_sol, save=True)
