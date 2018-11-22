92a90d2a6423790ce1d1c7f0a5f2885052027e40
Amanuel Wolde-Kidan <woldeaman@hiraiwamac.physik.fu-berlin.de>
2018-11-22 11:18:05 +0100

    adjusted to work with early stopping

--- post_processing/regularization.py
+++ post_processing/regularization.py
@@ -26,13 +26,14 @@ def compute_exp_data(path, name):
     return cc, xx, dxx_dist, dxx_width
 
 
-def read_results(path, alphas, xx, cc_all, dxx_dist, dxx_width, tt_fit, dt=10):
+def read_results(path, alphas, xx, cc_all, dxx_dist, dxx_width, tt_fit,
+                 subppath='alpha', dt=10):
     """Read data for all values of regularization parameter alpha."""
     err_sol, err_reg, parameters = [], [], []
     cc_exp = [cc_all[t//dt] for t in tt_fit]  # get fitted profiles only !
     for a in alphas:
         # read best fitted parameters
-        res = pd.HDFStore('%s/alpha_%s/results.h5' % (path, a))
+        res = pd.HDFStore('%s/%s_%s/results.h5' % (path, subppath, a))
         key_list = np.array(list(res.root._v_children.keys()))
         error = np.array([res[key]['cost'].values[0] for key in key_list])
         best_results = res[key_list[np.argmin(error)]+'/x'].values[:, 0]
@@ -53,7 +54,7 @@ def get_test_set_scalings(scalings, tt_fit, tt_test):
     """Interpolate fitted scaling values for test set profiles."""
     test_scalings = []
     for s in scalings:  # for each alpha interpolate scalings
-        spl = ip.UnivariateSpline(tt_fit[1:], s, s=0, k=1, ext='raise')
+        spl = ip.UnivariateSpline(tt_fit[1:], s, s=0, k=1, ext=0)
         f_test = spl(tt_test[1:])  # best guess for scalings of unfitted profiles...
         test_scalings.append(f_test)
     return test_scalings
@@ -124,12 +125,13 @@ def plot_scalings(scalings_fit, scalings_test, tt_fit, tt_test, alphas, save=Fal
 
 
 @mpltex.acs_decorator  # making acs-style figures
-def plot_test_set_err(alphas, test_err, sol_err, save=False):
+def plot_test_set_err(alphas, test_err, sol_err, xlabel='$\\alpha$',
+                      xscale='log', save=False):
     """Make plot for performance on test set over alphas."""
     plt.plot(alphas, test_err, 'k.--', label='test set')
     plt.plot(alphas, sol_err, 'r.--', label='fitting set')
-    plt.xscale('log')
-    plt.xlabel('$\\alpha$')
+    plt.xscale(xscale)
+    plt.xlabel(xlabel)
     plt.ylabel('$\sigma$')
     # legend
     plt.legend(frameon=False)
@@ -145,30 +147,40 @@ def plot_test_set_err(alphas, test_err, sol_err, save=False):
 ################################
 #    SETTING UP ENVIRONMENT    #
 ##########################################################################
-alphas = ['0', '0.000001', '0.00001', '0.00005', '0.0001', '0.0005', '0.001', '0.005', '0.01', '0.05', '0.1', '0.5']
+nfevs = [10, 25, 50, 75, 100]
 # NOTE: both tt-vectors should include t = 0 s !
-tt_fit = [0, 30, 60, 90, 120, 180, 240, 300, 330, 360, 390, 420, 480, 510, 540, 570, 630,
-          660, 720, 750, 780, 810, 870, 900, 930, 960, 990, 1020, 1050, 1080, 1110, 1140,
-          1170, 1200, 1230, 1260, 1290, 1350, 1380, 1410, 1440, 1470, 1530, 1590, 1620,
-          1680, 1710, 1740, 1770, 1830, 1860, 1890, 1920, 1950, 1980, 2010, 2040, 2070,
-          2100, 2130, 2160, 2220, 2250, 2280, 2310, 2370, 2400, 2460, 2490, 2520, 2550,
-          2580, 2610, 2640]
-tt_test = [0, 150, 210, 270, 450, 600, 690, 840, 1320, 1500, 1560, 1650, 1800, 2190, 2340, 2430]
-path = home+"/Desktop/Cluster/jobs/fokkerPlanckModel/PEG_dextran/7.Batch/regularization/gel10_dex70/"
+tt_fit = [0, 30, 60, 90, 210, 270, 300, 330, 360, 480, 510, 540, 570, 600, 630,
+          660, 690, 750, 780, 810, 840, 870, 900, 930, 960, 990, 1020, 1080, 1110,
+          1140, 1200, 1260, 1290, 1350, 1410, 1440, 1470, 1500, 1560, 1620, 1680,
+          1770, 1800, 1860, 1980, 2010, 2040, 2070, 2100, 2160, 2190, 2220, 2310,
+          2340, 2430, 2460, 2520, 2550, 2580, 2610]
+tt_test = [0, 120, 150, 180, 240, 390, 420, 450, 720, 1050, 1170, 1230,
+           1320, 1380, 1530, 1590, 1650, 1710, 1740, 1830, 1890, 1920, 1950,
+           2130, 2250, 2280, 2370, 2400, 2490, 2640]
+# alphas = ['0', '0.000001', '0.00001', '0.00005', '0.0001', '0.0005', '0.001', '0.005', '0.01', '0.05', '0.1', '0.5']
+# tt_fit = [0, 30, 60, 90, 120, 180, 240, 300, 330, 360, 390, 420, 480, 510, 540, 570, 630,
+#           660, 720, 750, 780, 810, 870, 900, 930, 960, 990, 1020, 1050, 1080, 1110, 1140,
+#           1170, 1200, 1230, 1260, 1290, 1350, 1380, 1410, 1440, 1470, 1530, 1590, 1620,
+#           1680, 1710, 1740, 1770, 1830, 1860, 1890, 1920, 1950, 1980, 2010, 2040, 2070,
+#           2100, 2130, 2160, 2220, 2250, 2280, 2310, 2370, 2400, 2460, 2490, 2520, 2550,
+#           2580, 2610, 2640]
+# tt_test = [0, 150, 210, 270, 450, 600, 690, 840, 1320, 1500, 1560, 1650, 1800, 2190, 2340, 2430]
+path = home+"/Desktop/Cluster/jobs/fokker_planck_modelling/Block_PEG/early_stopping/gel10_dex70"
 ##########################################################################
 
 
 #################################
 #             MAIN LOOP         #
 ##########################################################################
-cc_all, xx, dxx_dist, dxx_width = compute_exp_data(path+'/alpha_0/', 'gel10_dex70')
-err_sol, err_reg, parameters = read_results(path, alphas, xx, cc_all, dxx_dist, dxx_width, tt_fit, dt=30)
+cc_all, xx, dxx_dist, dxx_width = compute_exp_data(path+'/fev_100/', 'gel10_dex70')
+err_sol, err_reg, parameters = read_results(path, nfevs, xx, cc_all, dxx_dist,
+                                            dxx_width, tt_fit, subppath='fev', dt=30)
 
 scalings = [p[6:] for p in parameters]  # extract scaling factors from parameters
 test_scalings = get_test_set_scalings(scalings, tt_fit, tt_test)
 test_err = compute_test_performance(xx, dxx_dist, dxx_width, cc_all, parameters,
                                     tt_test, test_scalings, dt=30)
 
-plot_scalings(scalings, test_scalings, tt_fit, tt_test, alphas, save=True)
-plot_lcurve(np.array(alphas).astype(float), err_sol, err_reg, save=True)
-plot_test_set_err(np.array(alphas).astype(float), test_err, err_sol, save=True)
+plot_scalings(scalings, test_scalings, tt_fit, tt_test, nfevs, save=True)
+plot_lcurve(nfevs, err_sol, err_reg, save=True)
+plot_test_set_err(nfevs, test_err, err_sol, xlabel='nfev', xscale='linear', save=True)
