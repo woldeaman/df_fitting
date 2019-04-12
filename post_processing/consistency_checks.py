4344e0e8b15dad30ca9de1b0b602fd80a1279c35
Amanuel Wolde-Kidan <woldeaman@hiraiwamac.physik.fu-berlin.de>
2019-04-12 18:43:22 +0200

    adjusted for latest data

--- post_processing/consistency_checks.py
+++ post_processing/consistency_checks.py
@@ -88,15 +88,16 @@ path = '/Users/woldeaman/Dropbox/PhD/Projects/FokkerPlanckModeling/PEG_Gel/9.Bat
 #  MAIN LOOP    #
 ##########################################################################
 def main():
-    gels = [6, 10]
+    gels = [6, 10, 20]
     dextrans = {6: ['dex4', 'dex10', 'dex20', 'dex40', 'dex70'],
-                10: ['dex40']}  # dextrans measured for each gel
+                10: ['dex4', 'dex4_vol2', 'dex10', 'dex10_vol2', 'dex20', 'dex20_vol2', 'dex40', 'dex40_vol2'],
+                20: ['dex20', 'dex40']}  # dextrans measured for each gel
     # dt_setups = {g: {'dex4': 10, 'dex4_cut': 10, 'dex20': 10, 'dex40': 10, 'FITC': 10, 'dex70': 30} for g in gels}
     for g in gels:
         for d in dextrans[g]:
             # plotting profiles
             path_p = path+'/gel%i_%s' % (g, d)  # path to experimental profiles
-            # path_p = "/Users/woldeaman/Desktop/"  # path to experimental profiles
+            path_p = "/Users/woldeaman/Desktop/"  # path to experimental profiles
             data = np.loadtxt(path_p+'/gel%i_%s.txt' % (g, d), delimiter=',')  # read profile data
             # dt = dt_setups[g][d]  # intervall between recorded stacks
             dt = 10
@@ -110,7 +111,6 @@ def main():
             cc_theo = np.loadtxt(path_res+'/cc_theo_best.txt', delimiter=',')
             residuals = [c_e - c_t[6:] for c_e, c_t in zip(cc_scaled[1:], cc_theo[:, 1:].T)]
             plot_residuals(xx, residuals, tt, t_sig, save=True, savePath='/Users/woldeaman/Desktop/', name='gel%s_%s_residuals' % (g, d))
-
 
 if __name__ == "__main_":
     main()
